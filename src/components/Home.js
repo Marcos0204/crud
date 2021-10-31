718ec03af0752bb384d2838bf3a4375cc4bef2c9
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-31 22:50:19 +0100

    structure-login-register

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = ( ) => {
     return (
-        <h1>Hokdjsfhksjdfhksd/h1>
+        <h1>hoome</h1>
     )   
 }
 
--- src/components/Logueo.js
+++ src/components/Logueo.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Stack, Container, Form, Button } from "react-bootstrap";
 
 const Logueo = ( ) => {
+    const [ iseRegister, SetIsRegister ] = useState(false)
+
+
+    async function submitHandler(e) {
+        e.preventDefault();
+        const email= e.target.formBasicEmail.value;
+        const password = e.target.formBasicPassword.value;
+        
+    }
     return (
-        <h1>Logueo</h1>
-    )   
+    
+        <Container>
+            <Stack gap={3}>
+                <h1>{iseRegister ? 'Rigistrate' : 'inicia sesion'}</h1>
+                <Form 
+                    onSubmit={submitHandler}
+                >
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+    
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    
+                    <Button variant="dark" type="submit">
+                        {iseRegister ? 'Rigistrate' : 'inicia sesion'}
+                    </Button>
+                </Form>
+                <Button
+                    variant="primary"
+                    type="submit"
+                    style={{ width: "300px" }}
+                    //onClick={()=> signInWithRedirect(auth, googleAuthProvider)}
+                >
+                    Acceder con Google
+                </Button>
+                <Button variant="secondary" onClick={() => SetIsRegister(!iseRegister)} style={{ width: "300px" }}>
+                {iseRegister ? '¿No tienes cuanta? Rigistrate' : '¿Ya tienes cuenta? inicia sesion'}
+                </Button>
+            </Stack>
+        </Container>
+        
+      );   
 }
 
 export default Logueo;
