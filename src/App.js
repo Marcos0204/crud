196ad9dbe6d79415cce0e5ced40c6b697f3e96d8
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-01 01:25:19 +0100

    auth-global

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { useEffect, useState } from 'react';
 import Home from './components/Home';
 import Logueo from './components/Logueo';
 
+import {
+  getAuth,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import {app} from './firebase';
+const Auth = getAuth(app);
+
 const App= () => {
   const [ userGlobal, setUserGlobal ] = useState(false);
-
+  onAuthStateChanged(Auth, (userAuth) =>{
+    if(userAuth){
+      setUserGlobal(userAuth)
+    } else {
+      setUserGlobal(null);
+    }
+  } )
   return (
     <div className="App">
       {userGlobal ? <Home/> : <Logueo />}
